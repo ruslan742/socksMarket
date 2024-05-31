01a049005848b94772029a2719c835dd4e9f0667
InnaGennadievna666 <inna-tretnova@mail.ru>
2024-05-31 13:32:45 +0300

    ololo

--- server/db/seeders/20240529161819-image.js
+++ server/db/seeders/20240529161819-image.js
@@ -5,40 +5,40 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Images', [
       {
-        name: 'https://content.podarki.ru/goods-images/10ac8777-fad0-45fb-a155-c974dd55905b.jpg',
+        name: 'https://gas-kvas.com/grafic/uploads/posts/2024-01/gas-kvas-com-p-babochka-dlya-detei-na-prozrachnom-fone-3.png',
       },
       {
-        name: 'https://kukla-sharzh.ru/wp-content/uploads/2016/10/114-830x1024.jpg',
+        name: 'https://pngicon.ru/file/uploads/malina.png',
       },
       {
         name: 'https://www.conphetti.ru/upload/iblock/842/7l36l3splkd1u1vpftn0hfe92hzv6itu.jpg',
       },
       {
-        name: 'https://pibig.info/uploads/posts/2022-12/1670595462_1-pibig-info-p-podelki-iz-plastilina-vozdushnogo-zhivotni-1.jpg',
+        name: 'https://pngicon.ru/file/uploads/ezhik2.png',
       },
       {
-        name: 'https://w-dog.ru/wallpapers/14/3/515585793483105/leopard-glaza-golubye-okras-lezhit.jpg',
+        name: 'https://catherineasquithgallery.com/uploads/posts/2021-02/1612851481_2-p-krasnaya-mashina-bez-fona-2.png',
       },
       {
-        name: 'https://www.w-dog.ru/wallpapers/0/10/318536220608052/milye-sozdaniya-svetlenkij-shhenok-i-polosatenkij-kotenok-druzhat-drug-s-drugom.jpg',
+        name: 'https://catherineasquithgallery.com/uploads/posts/2021-03/1614589728_21-p-kartinka-sobaki-na-belom-fone-23.png',
       },
       {
         name: 'https://w.forfun.com/fetch/29/2942cda3da91073bcaf9915bec9195d5.jpeg',
       },
       {
-        name: 'https://i.pinimg.com/originals/c4/4a/74/c44a74f0553bcf43d178c64a65d52779.jpg',
+        name: 'https://www.pngarts.com/files/3/Flying-Eagle-PNG-Picture.png',
       },
       {
-        name: 'https://klike.net/uploads/posts/2022-08/1661403869_j-21.jpg',
+        name: 'https://abrakadabra.fun/uploads/posts/2022-03/1646501838_1-abrakadabra-fun-p-klipart-romashka-na-prozrachnom-fone-1.png',
       },
       {
-        name: 'https://model.gdebirka.ru/image/data/drift/drift-car-zyk-k2669-5.jpg',
+        name: 'https://www.funnyart.club/uploads/posts/2022-12/thumbs/1671647742_www-funnyart-club-p-kartinki-bez-fona-besplatno-krasivie-25.png',
       },
       {
         name: 'https://gas-kvas.com/uploads/posts/2023-02/1675462324_gas-kvas-com-p-fonovii-risunok-robot-16.png',
       },
       {
-        name: 'https://get.pxhere.com/photo/sweet-flower-cute-decoration-green-frog-amphibian-yellow-fauna-deco-tree-frog-close-up-crown-fun-figure-vertebrate-funny-macro-photography-frog-prince-800991.jpg',
+        name: 'https://catherineasquithgallery.com/uploads/posts/2021-03/1614554211_9-p-rozi-kartinki-na-belom-fone-16.png',
       },
    ], {});
   },
